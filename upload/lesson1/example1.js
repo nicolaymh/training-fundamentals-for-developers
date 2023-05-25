7d8b45483c05c842354b4bd67c53a07014353c9a
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-24 20:26:53 -0500

    Creating index.js +

--- upload/lesson1/example1.js
+++ upload/lesson1/example1.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-console.log(require('dotenv').config());
-cloudinary.uploader.upload('assets/images/kitten.jpg')
-    .then(uploadResult => console.log(JSON.stringify(uploadResult, null, 2)))
-    .catch(error => console.error(error));
+import dotenv from "dotenv";
+import { v4 as cloudinary } from "cloudinary";
+
+dotenv.config();
+
+cloudinary.uploader
+   .upload("assets/images/kitten.jpg")
+   .then((uploadResult) => console.log(JSON.stringify(uploadResult, null, 2)))
+   .catch((error) => console.error(error));
