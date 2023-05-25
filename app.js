1bb5d61289de8d52e7a347a925ac0a3ec205eaea
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-24 21:35:50 -0500

    Lesson1: Example5 ok

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import {
    uploadExample2,
    uploadExample3,
    uploadExample4,
+   uploadExample5,
 } from "./upload/lesson1/index.js";
 
 // uploadExample1();
 // uploadExample2();
 // uploadExample3();
 // uploadExample4();
+uploadExample5();
--- upload/lesson1/index.js
+++ upload/lesson1/index.js
@@ -2,5 +2,6 @@ import uploadExample1 from "./example1.js";
 import uploadExample2 from "./example2.js";
 import uploadExample3 from "./example3.js";
 import uploadExample4 from "./example4.js";
+import uploadExample5 from "./example5.js";
 
-export { uploadExample1, uploadExample2, uploadExample3, uploadExample4 };
+export { uploadExample1, uploadExample2, uploadExample3, uploadExample4, uploadExample5 };
